0b3416533d514096f20c2fb86629e2ff4907034f
daobt <buidao1703@gmail.com>
2025-10-27 11:13:41 +0700

    test add function

---
+++ common/theme/lib/theme.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ButtonView extends StatelessWidget {
+  const ButtonView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.amber,
+      padding: EdgeInsets.all(8),
+      child: Text("HUHU"),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:check_app/profile.dart';
 import 'package:flutter/material.dart';
+import 'package:home/home.dart';
+import 'package:profile/profile.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +12,30 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
+    return MaterialApp(
+      home: Demo(),
+    );
+  }
+}
+
+class Demo extends StatelessWidget {
+  const Demo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => ChatApp()),
+            );
+          },
           child: Text('Hello World!'),
         ),
       ),
     );
   }
 }
+
